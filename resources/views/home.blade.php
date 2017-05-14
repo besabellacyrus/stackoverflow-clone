86681d9424eecbb9fcede1a141c0fb171b29b49c
Cyrus <besabellacyrus@gmail.com>
2017-05-15 07:55:29 +0800

    fixing topic navigation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,29 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+        <div class="col-md-8">
+            <div class="posts-area">
+                <ul class="topic-nav pull-right">
+                    <li class="active">
+                        <a href="">interested</a>
+                    </li>
+                    <li>
+                        <a href="">featured</a>
+                    </li>
+                    <li>
+                        <a href="">hot</a>
+                    </li>
+                    <li>
+                        <a href="">week</a>
+                    </li>
+                    <li>
+                        <a href="">month</a>
+                    </li>
+                </ul>
             </div>
+        </div>
+        <div class="col-md-4">
+            sidebar?
         </div>
     </div>
 </div>
